4ae252eb4caa70646f23e9de41459398632fc6a4
Lucas Morais <lucasmorais@clubegazetadopovo.com.br>
2022-05-12 23:43:48 -0300

    feat: add simple button

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -13,3 +13,14 @@ export function Button({title, onPress, ...rest}) {
     </TouchableOpacity>
   );
 }
+export function SimpleButton({title, onPress, ...rest}) {
+  return (
+    <TouchableOpacity
+      style={styles.simpleContainer}
+      activeOpacity={0.7}
+      onPress={onPress}
+      {...rest}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
